cfc844b9d5b4da883e056ebb61394d5df19fb0d5
Vincent Le Biannic <850046+Lyrkan@users.noreply.github.com>
2017-08-24 17:38:00 +0200

    Fix indent of webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,16 @@ const Encore = require('@symfony/webpack-encore');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 Encore
-    .setOutputPath('build/')
-    .setPublicPath('/')
-    .cleanupOutputBeforeBuild()
-    .addEntry('main', './src/index.js')
-    .addStyleEntry('styles', './src/index.scss')
-    .enableSassLoader()
-    .enablePreactPreset()
-    .addPlugin(new CopyWebpackPlugin([
-      { from: './src/index.html', to: 'index.html' }
-    ]))
+  .setOutputPath('build/')
+  .setPublicPath('/')
+  .cleanupOutputBeforeBuild()
+  .addEntry('main', './src/index.js')
+  .addStyleEntry('styles', './src/index.scss')
+  .enableSassLoader()
+  .enablePreactPreset()
+  .addPlugin(new CopyWebpackPlugin([
+    { from: './src/index.html', to: 'index.html' }
+  ]))
 ;
 
 module.exports = Encore.getWebpackConfig();
