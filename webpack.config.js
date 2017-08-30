4db2e81e1e9dcdd0ecb045aa32d178c7e5163117
Vincent LE BIANNIC <vincent.le-biannic@niji.fr>
2017-08-30 13:41:51 +0200

    Add Browsersync example

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 // webpack.config.js
 const Encore = require('@symfony/webpack-encore');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 Encore
@@ -14,5 +15,12 @@ Encore
     { from: './src/index.html', to: 'index.html' }
   ]))
 ;
+
+if (!Encore.isProduction()) {
+  Encore.addPlugin(new BrowserSyncPlugin(
+    { proxy: 'http://localhost:8080' },
+    { reload: false }
+  ));
+}
 
 module.exports = Encore.getWebpackConfig();
